491e3cfce111d852ebfd932553a47fad4974ccb0
hurynovich <8087963+hurynovich@users.noreply.github.com>
2021-09-08 13:45:23 +0300

    'base' project was modularized

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -2,8 +2,14 @@ plugins {
     `java-library`
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
 dependencies {
     compileOnly(DepsCatalog.lombok)
+    annotationProcessor(DepsCatalog.lombok)
 
     testImplementation(DepsCatalog.junitApi)
     testRuntimeOnly(DepsCatalog.junitEngine)
---
+++ base/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module viola.jones.base {
+    requires java.desktop;
+    requires static lombok;
+}
