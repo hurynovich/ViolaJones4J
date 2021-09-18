1a05ce8e82cd6a895fa4727c8e14f3d38ad469ea
hurynovich <8087963+hurynovich@users.noreply.github.com>
2021-09-19 01:28:28 +0300

    enhanced Rect class

--- detector-api/src/main/java/io/github/hurynovich/vj4j/detector/api/Rect.java
+++ detector-api/src/main/java/io/github/hurynovich/vj4j/detector/api/Rect.java
@@ -1,9 +1,14 @@
 package io.github.hurynovich.vj4j.detector.api;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 @AllArgsConstructor
+@EqualsAndHashCode
 @Getter
 public final class Rect {
     public final Int2D a;
@@ -28,5 +33,20 @@ public final class Rect {
 
     public Rect translate(Int2D shift) {
         return new Rect(a.plus(shift), b.plus(shift));
+    }
+
+    public static Rect intersection(Rect one, Rect two){
+        int aX = max(one.a.x, two.a.x);
+        int aY = max(one.a.y, two.a.y);
+        int bX = min(one.b.x, two.b.x);
+        int bY = min(one.b.y, two.b.y);
+
+        if (aX > bX || aY > bY) return null;
+
+        return new Rect(aX, aY, bX, bY);
+    }
+
+    public static Rect rect(int x1, int y1, int x2, int y2){
+        return new Rect(x1, y1, x2, y2);
     }
 }
