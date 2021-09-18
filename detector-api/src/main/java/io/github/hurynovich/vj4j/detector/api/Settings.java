93c9cffaaf1c8123e0aa9aa63a970b519cee29f5
hurynovich <8087963+hurynovich@users.noreply.github.com>
2021-09-19 01:19:39 +0300

    created initial class holding settings for Detector

---
+++ detector-api/src/main/java/io/github/hurynovich/vj4j/detector/api/Settings.java
@@ -0,0 +1,11 @@
+package io.github.hurynovich.vj4j.detector.api;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Settings {
+    private Int2D minObjectSize;
+    private Int2D maxObjectSize;
+    private int minNeighbors;
+}
