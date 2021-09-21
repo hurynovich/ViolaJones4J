78e8ff0809586cbfa0f205c7bdef92b64b9fbb8f
hurynovich <8087963+hurynovich@users.noreply.github.com>
2021-09-21 13:17:35 +0300

    adjusted dependencies for 'commons' module

--- commons/build.gradle.kts
+++ commons/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
 }
 
 dependencies {
+    implementation(project(":detector-api"))
+
     testImplementation(DepsCatalog.junitApi)
     testImplementation(DepsCatalog.junitParams)
     testRuntimeOnly(DepsCatalog.junitEngine)
---
+++ commons/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module vj4j.commons {
+    requires java.desktop;
+    requires vj4j.detector.api;
+
+    exports io.github.hurynovich.vj4j.commons;
+}
