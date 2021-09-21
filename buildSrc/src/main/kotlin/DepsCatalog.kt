e8256a11e3bc4d4d02e3200c5f74a0b955f7f03b
hurynovich <8087963+hurynovich@users.noreply.github.com>
2021-09-21 10:43:27 +0300

    picocli-codegen was added to DepsCatalog

--- buildSrc/src/main/kotlin/DepsCatalog.kt
+++ buildSrc/src/main/kotlin/DepsCatalog.kt
@@ -13,6 +13,7 @@ object DepsCatalog {
     const val loggerApi = "org.slf4j:slf4j-api:1.7.32"
     const val loggerImpl = "org.slf4j:slf4j-simple:1.7.32"
     const val picocli = "info.picocli:picocli:4.6.1"
+    const val picocliCodegen = "info.picocli:picocli-codegen:4.6.1"
     const val junitApi = "org.junit.jupiter:junit-jupiter-api:${Versions.junit}"
     const val junitEngine = "org.junit.jupiter:junit-jupiter-engine:${Versions.junit}"
     const val junitParams = "org.junit.jupiter:junit-jupiter-params:${Versions.junit}"
