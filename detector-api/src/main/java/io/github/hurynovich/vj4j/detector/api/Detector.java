6fec806867b16f15264f708d83c235d42fe6e3ad
hurynovich <8087963+hurynovich@users.noreply.github.com>
2021-10-03 15:43:48 +0300

    enhanced Detector interface

--- detector-api/src/main/java/io/github/hurynovich/vj4j/detector/api/Detector.java
+++ detector-api/src/main/java/io/github/hurynovich/vj4j/detector/api/Detector.java
@@ -3,5 +3,5 @@ package io.github.hurynovich.vj4j.detector.api;
 import java.util.List;
 
 public interface Detector {
-    List<Rect> detect(Image image, Settings settings) throws DetectorException;
+    List<? extends Rect> detect(Image image, Settings settings) throws DetectorException;
 }
