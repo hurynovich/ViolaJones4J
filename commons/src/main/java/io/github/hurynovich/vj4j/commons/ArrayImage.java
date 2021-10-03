972a7b3869da949bf9bebc40f782240e59916f00
hurynovich <8087963+hurynovich@users.noreply.github.com>
2021-10-03 15:44:34 +0300

    saved experimental implementation of Image

---
+++ commons/src/main/java/io/github/hurynovich/vj4j/commons/ArrayImage.java
@@ -0,0 +1,42 @@
+package io.github.hurynovich.vj4j.commons;
+
+import io.github.hurynovich.vj4j.detector.api.Image;
+import io.github.hurynovich.vj4j.detector.api.Point;
+
+//TODO experimental class
+@Deprecated(forRemoval = false)
+public class ArrayImage implements Image {
+    private final int[][] data;
+
+    ArrayImage(Point size){
+        this(size.getX(), size.getY());
+    }
+
+    ArrayImage(int width, int height){
+        data = new int[height][width];
+    }
+
+    public void setRGB(int val, int x, int y) {
+        data[y][x] = val;
+    }
+
+    @Override
+    public int getRGB(int x, int y) {
+        return data[y][x];
+    }
+
+    @Override
+    public int getWidth() {
+        return data[0].length;
+    }
+
+    @Override
+    public int getHeight() {
+        return data.length;
+    }
+
+    @Override
+    public Image scale(Point targetSz) {
+        return null;
+    }
+}
