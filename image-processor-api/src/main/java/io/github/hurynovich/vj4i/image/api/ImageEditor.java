7e037f7365cc2d23e5b283710d56dbd48d02c7ae
hurynovich <8087963+hurynovich@users.noreply.github.com>
2021-10-03 15:15:11 +0300

    created module 'image-processor-api'

---
+++ image-processor-api/src/main/java/io/github/hurynovich/vj4i/image/api/ImageEditor.java
@@ -0,0 +1,18 @@
+package io.github.hurynovich.vj4i.image.api;
+
+import io.github.hurynovich.vj4j.core.api.Image;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
+
+//TODO javadoc
+public interface ImageEditor {
+    Image read(Path source);
+    Image read(InputStream source);
+    void write(Image img, Path target);
+    void write(OutputStream target);
+    Image newImage(int width, int height);
+    Image scale(Image image, int targetWidth, int targetHeight);
+    void drawLine(Image image, int x1, int y1, int x2, int y2, int color);
+}
---
+++ image-processor-api/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module vj4j.image.processor.api {
+    requires transitive vj4j.core.api;
+
+    exports io.github.hurynovich.vj4i.image.api;
+}
