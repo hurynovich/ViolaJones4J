c766f38af8476fb56cb710ed3c65981b1d5fd209
hurynovich <8087963+hurynovich@users.noreply.github.com>
2021-10-25 14:57:38 +0300

    rearranged subprojects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,14 @@
-rootProject.name = "vj4j-parent"
+rootProject.name = "violajones4j"
+
+include("core-api")
+include("commons")
+
+include("image-processor-api")
+include("image-processor-awt-impl")
 
 include("detector-api")
 include("detector-cli")
 include("detector-opencv-impl")
+include("detector-swing-gui")
 
 include("trainer-api")
-include("commons")
-include("image-processor-api")
-include("image-processor-awt-impl")
-include("core-api")
